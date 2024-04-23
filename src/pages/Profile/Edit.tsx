feaabec75868daee6cbbc14864b7558d23c9c91b
Aakarsh-Sathish <aakarshsathish@gmail.com>
2024-04-23 18:50:23 -0400

    Brief and Comments on code

--- src/pages/Profile/Edit.tsx
+++ src/pages/Profile/Edit.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import './Edit.css'; 
-import { Button } from 'react-bootstrap';
+import './Edit.css'; // Importing custom CSS styles
+import { Button } from 'react-bootstrap'; // Importing Button component from react-bootstrap
 
+// Define initial form values and validation schema using Yup
 const Edit: React.FC = () => {
   const initialValues = {
     fullName: '',
@@ -32,29 +33,32 @@ const Edit: React.FC = () => {
     handle: Yup.string().required('Handle is required'),
   });
 
+  // Handle form submission
   const handleSubmit = (values: any, { setSubmitting }: any) => {
     setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
+      alert(JSON.stringify(values, null, 2)); // Display form values as JSON
       setSubmitting(false);
     }, 400);
   };
 
   return (
-    <div className="edit-form-container">
-      <h2 className='h2-user-profile'>User Profile Information</h2>
+    <div className="edit-form-container"> {/* Container for the entire form */}
+      <h2 className='h2-user-profile'>User Profile Information</h2> {/* Heading for user profile */}
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
-        {({ isSubmitting }) => (
-          <Form>
+        {({ isSubmitting }) => ( // Formik render prop function
+          <Form> {/* Form component */}
+            {/* Form fields with labels, inputs, and error messages */}
             <div className="form-field">
               <label htmlFor="fullName" style={{ fontWeight: 800 }}>Full name (last, first[middle]):</label>
               <Field type="text" name="fullName" />
               <ErrorMessage name="fullName" component="div" className="error-message" />
             </div>
 
+            {/* Password and Confirm Password fields with validation */}
             <div className="form-field">
               <label htmlFor="password">Password:</label>
               <Field type="password" name="password" />
@@ -67,16 +71,19 @@ const Edit: React.FC = () => {
               <ErrorMessage name="confirmPassword" component="div" className="error-message" />
             </div>
 
+            {/* Note regarding password field */}
             <div className='italics'>
-                <p>If password field is blank, the password will not be updated</p>
+              <p>If password field is blank, the password will not be updated</p>
             </div>
 
+            {/* Email field */}
             <div className="form-field">
               <label htmlFor="email" style={{ fontWeight: 800 }}>E-mail address:</label>
               <Field type="email" name="email" />
               <ErrorMessage name="email" component="div" className="error-message" />
             </div>
 
+            {/* Institution field */}
             <div className="form-field">
               <label htmlFor="institution" style={{ fontWeight: 800 }}>Institution:</label>
               <Field as="select" name="institution">
@@ -84,6 +91,7 @@ const Edit: React.FC = () => {
               </Field>
             </div>
 
+            {/* Action Preference radio buttons */}
             <div className="form-field action-preference custom-column-flex">
               <label style={{ fontWeight: 800 }}>Action Preference:</label>
               <div className="radio-group">
@@ -99,22 +107,23 @@ const Edit: React.FC = () => {
               <ErrorMessage name="actionPreference" component="div" className="error-message" />
             </div>
 
-            <hr />
+            <hr /> {/* Horizontal rule for visual separation */}
 
+            {/* Handle field with instructions */}
             <div className='custom-column-flex'>
-                <label style={{ fontWeight: 800 }}>Handle</label>
-                <div>A "handle" can be used to conceal your username from people who view your wiki pages. If you have a handle, your wiki account should be named after your handle instead of after your user-ID. If you do not have a handle, your Expertiza user-ID will be used instead. A blank entry in the field below will cause the handle to be set back to your Expertiza user-ID. <div style={{ marginTop: -12 }}><br /></div>
+              <label style={{ fontWeight: 800 }}>Handle</label>
+              <div>A "handle" can be used to conceal your username from people who view your wiki pages. If you have a handle, your wiki account should be named after your handle instead of after your user-ID. If you do not have a handle, your Expertiza user-ID will be used instead. A blank entry in the field below will cause the handle to be set back to your Expertiza user-ID. <div style={{ marginTop: -12 }}><br /></div>
                 Note: By using this form, you are changing your default handle, which will be used for all future assignments. To change your handle for a specific assignment, select that assignment and choose the Change Handle action.</div>
             </div>
 
+            {/* Handle input field */}
             <div className="form-field" style={{ marginTop: 28 }}>
               <label htmlFor="handle">Default Handle:</label>
               <Field type="text" name="handle" />
               <ErrorMessage name="handle" component="div" className="error-message" />
             </div>
 
-           
-
+            {/* Email Options checkboxes */}
             <div className="email-options-container">
               <div className="email-options-header">
                 <label className="email-options-heading">Email Options:</label>
@@ -136,7 +145,7 @@ const Edit: React.FC = () => {
               </div>
             </div>
 
-
+            {/* Preferred Time Zone field */}
             <div className="form-field">
               <label htmlFor="timeZone">Preferred Time Zone:</label>
               <Field as="select" name="timeZone">
@@ -144,6 +153,7 @@ const Edit: React.FC = () => {
               </Field>
             </div>
 
+            {/* Preferred Language field */}
             <div className="form-field">
               <label htmlFor="language">Preferred Language:</label>
               <Field as="select" name="language">
@@ -151,6 +161,7 @@ const Edit: React.FC = () => {
               </Field>
             </div>
 
+            {/* Submit button */}
             <div className="form-field">
               <Button type="submit" disabled={isSubmitting} variant="outline-success">
                 Save
